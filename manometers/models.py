058e7d05c204e330ab224720454e7f9e492fbf48
kipishbringer <kipishbringer@gmail.com>
2026-05-16 13:18:05 +0700

    beginning

--- manometers/models.py
+++ manometers/models.py
@@ -5,6 +5,77 @@ from users.models import User
 NULLABLE = {'blank': True, 'null': True}
 
 
+class Subdivision(models.Model):
+    name = models.CharField(max_length=30, unique=True, verbose_name="название")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subdivisions')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'подразделение'
+        verbose_name_plural = 'подразделения'
+
+
+class Brigade(models.Model):
+    name = models.CharField(max_length=100, verbose_name="название")
+    subdivision = models.ForeignKey(Subdivision, on_delete=models.CASCADE, related_name='brigades')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'бригада'
+        verbose_name_plural = 'бригады'
+
+
+class Territory(models.Model):
+    TERRITORY_TYPES = [
+        ('A', 'A'),
+        ('B', 'B'),
+        ('C', 'C'),
+        ('D', 'D'),
+        ('GENERAL', 'Общая'),
+    ]
+
+    name = models.CharField(max_length=255, verbose_name='название')
+    territory_type = models.CharField(max_length=20, choices=TERRITORY_TYPES)
+    subdivision = models.ForeignKey(Subdivision, on_delete=models.CASCADE, related_name='territories')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'территория'
+        verbose_name_plural = 'территории'
+
+
+class BrigadeTerritoryAssignment(models.Model):
+    brigade = models.ForeignKey(Brigade, on_delete=models.CASCADE, related_name='territory_assignments',
+                                verbose_name="бригада")
+    territory = models.ForeignKey(Territory, on_delete=models.CASCADE, related_name='brigade_assignments',
+                                  verbose_name="территория")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.brigade.name} принадлежит {self.territory.name}'
+
+    class Meta:
+        verbose_name = 'принадлежность'
+        verbose_name_plural = 'принадлежности'
+
+"""ПОЗИЦИИ НАДО СДЕЛАТЬ"""
+
+
+
+
+
+
+
+
+"""ТРЕШ"""
 class Position(models.Model):
 
     class Group(models.TextChoices):
